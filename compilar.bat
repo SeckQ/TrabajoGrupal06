a0369d04431d2303d1e5f7e6cad8314279377433
SeckQ <45026680+SeckQ@users.noreply.github.com>
2023-02-26 21:44:12 -0500

    Add files via upload

--- compilar.bat
+++ compilar.bat
@@ -1,2 +1,2 @@
-git clone https://github.com/SeckQ/TrabajoGrupal06.git && cd ./TrabajoGrupal06 && cd ./app-books && gradlew libertyPackage && docker build -t seckq/app-books:1.0.0 . && cd .. && cd ./app-authors && docker build -t seckq/app-authors:1.0.0 . && cd .. && docker push seckq/app-books:1.0.0 && docker push seckq/app-authors:1.0.0 && docker compose up
+git clone https://github.com/SeckQ/TrabajoGrupal06.git && cd ./TrabajoGrupal06 && cd ./app-books && gradlew libertyPackage && docker build -t seckq/app-books:cuascota-1.0.0 . && cd .. && cd ./app-authors && docker build -t seckq/app-authors:cuascota-1.0.0 . && cd .. && docker push seckq/app-books:1.0.0 && docker push seckq/app-authors:1.0.0 && docker compose up
 
